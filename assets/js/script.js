ce938a4973c4d9b6b8f472e267169145057f9a09
Luke Carl Hartman <luke@lukecarlhartman.com>
2023-06-22 18:28:18 -0700

    sidebar works great!

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,33 @@
 	var toggle = document.querySelector(".sidebar-toggle")
 	var sidebar = document.querySelector("#sidebar")
 	var checkbox = document.querySelector("#sidebar-checkbox")
-
+	var widths = [0, 500, 1000]
+	if (window.innerWidth > widths[2]) {
+		console.log("object")
+		checkbox.setAttribute("checked", "checked")
+	}
 	document.addEventListener(
 		"click",
 		function (e) {
 			var target = e.target
 
-			if (
-				!checkbox.checked ||
-				sidebar.contains(target) ||
-				target === checkbox ||
-				target === toggle
-			)
-				return
+			function resizeFn() {
+				if (window.innerWidth < widths[2]) {
+					console.log("small screen")
+					// checkbox.checked = false
+					if (
+						!checkbox.checked ||
+						sidebar.contains(target) ||
+						target === checkbox ||
+						target === toggle
+					)
+						return
 
-			checkbox.checked = false
+					checkbox.checked = false
+				}
+			}
+			window.onresize = resizeFn
+			resizeFn()
 		},
 		false
 	)
